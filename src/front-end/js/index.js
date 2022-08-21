e9f6a5e73b370e96eb1409f4e8620b6d06feb47a
RugerClaus <rogerrock12games@gmail.com>
2022-08-21 13:47:54 -0600

    BoilerPlate2

--- src/front-end/js/index.js
+++ src/front-end/js/index.js
@@ -1,3 +1,71 @@
-const footer = document.getElementById('footer')
 const today = new Date()
-footer.innerHTML = `Copyright &copy; Recipays ${today.getFullYear()}`
+
+const header = () => {
+
+    const colors = {
+        sunday: () => {
+            banner.style.backgroundColor = 'blue'
+            banner.style.color = 'White'
+        },
+        monday: () => {
+            banner.style.backgroundColor = 'white'
+            banner.style.color = 'black'
+        },
+        tuesday: () => {
+            banner.style.backgroundColor = 'red'
+            banner.style.color = 'White'
+        },
+        wednesday: () => {
+            banner.style.backgroundColor = 'green'
+            banner.style.color = 'White'
+        },
+        thursday: () => {
+            banner.style.backgroundColor = 'black'
+            banner.style.color = 'White'
+        },
+        friday: () => {
+            banner.style.backgroundColor = 'yellow'
+            banner.style.color = 'black'
+        },
+        saturday: () => {
+            banner.style.backgroundColor = 'purple'
+            banner.style.color = 'White'
+        }
+    }
+
+    const banner = document.getElementById('banner')
+    switch (today.getDay()){
+        case 0: 
+            colors.sunday()
+            break
+        case 1: 
+            colors.monday()
+            break
+        case 2: 
+            colors.tuesday()
+            break
+        case 3:
+            colors.wednesday()
+            break
+        case 4:
+            colors.thursday()
+            break
+        case 5:
+            colors.friday()
+            break
+        case 6:
+            colors.saturday()
+            break
+    }
+}
+
+const footer = () => {
+    const footer = document.getElementById('footer')
+    footer.innerHTML = `Copyright &copy; Recipays ${today.getFullYear()}`
+}
+
+
+
+
+header()
+footer()
